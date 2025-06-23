24d0da286cb7ff1666d3bf8c28d004b0e160ebff
7snmsn <hsn688831@gmail.com>
2025-06-23 16:30:24 +0300

    deleted the addithion

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    cout << "Hello world! im hassan and this is my first test" << endl;
+    
+   
+   
+    cout  << "Hello world! im hassan and this is my first test" << endl;
     return 0;
+
 }
